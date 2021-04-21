9b5166f79cfc8baee90725754eab4f41f1928f9b
Kevin Casey <thomaskevincasey74@gmail.com>
2021-04-21 10:36:00 -0600

    Fixed another syntax bug

--- src/util/getOktaAccountInfo.ts
+++ src/util/getOktaAccountInfo.ts
@@ -1,13 +1,12 @@
 import { OktaAccountInfo } from '../okta/types';
 import { OktaIntegrationConfig } from '../types';
-import { IntegrationConfig } from '../config';
-import { IntegrationInstance } from '@jupiterone/integration-sdk-core';
 
 const DOMAIN_REGEX = /\/\/([^.]*).okta(preview)?.com/;
 
-export default function getOktaAccountInfo(
-  integrationInstance: IntegrationInstance<IntegrationConfig>,
-): OktaAccountInfo {
+export default function getOktaAccountInfo(integrationInstance: {
+  name: string;
+  config: object;
+}): OktaAccountInfo {
   const config = integrationInstance.config as OktaIntegrationConfig;
   const match = DOMAIN_REGEX.exec(config.oktaOrgUrl);
   if (!match) {
