0444c0ffdd0aa8c887fc150c6c6d3e83dfd9ed6d
Adam Pierson <adam.pierson@jupiterone.com>
2022-07-25 15:41:20 -0500

    Additional cleanup.  Removing additional gitleaks file.

--- src/steps/roles.ts
+++ src/steps/roles.ts
@@ -8,7 +8,36 @@ import {
 } from '@jupiterone/integration-sdk-core';
 import { createAPIClient } from '../client';
 import { IntegrationConfig } from '../config';
+import { OktaRole } from '../okta/types';
 import { Entities, Relationships, Steps } from './constants';
+
+function generateRoleKey(role: OktaRole) {
+  return Entities.ROLE._type + ':' + role.label + ':' + role.assignmentType;
+}
+
+function createRoleEntity(role: OktaRole) {
+  return createIntegrationEntity({
+    entityData: {
+      source: role,
+      assign: {
+        _key: generateRoleKey(role),
+        _type: Entities.ROLE._type,
+        _class: Entities.ROLE._class,
+        id: role.id,
+        description: role.description,
+        name: role.label,
+        displayName: role.label,
+        roleType: role.type,
+        status: role.status, //example: 'ACTIVE' or 'INACTIVE'
+        superAdmin: role.type === 'SUPER_ADMIN',
+        created: parseTimePropertyValue(role.created)!,
+        createdOn: parseTimePropertyValue(role.created)!,
+        lastUpdated: parseTimePropertyValue(role.lastUpdated)!,
+        lastUpdatedOn: parseTimePropertyValue(role.lastUpdated)!,
+      },
+    },
+  });
+}
 
 export async function fetchRoles({
   instance,
@@ -23,37 +52,9 @@ export async function fetchRoles({
     },
     async (user) => {
       await apiClient.iterateRolesByUser(user._key, async (role) => {
-        let roleEntity = await jobState.findEntity(
-          Entities.ROLE._type + ':' + role.label + ':' + role.assignmentType,
-        );
+        let roleEntity = await jobState.findEntity(generateRoleKey(role));
         if (!roleEntity) {
-          roleEntity = await jobState.addEntity(
-            createIntegrationEntity({
-              entityData: {
-                source: role,
-                assign: {
-                  _key:
-                    Entities.ROLE._type +
-                    ':' +
-                    role.label +
-                    ':' +
-                    role.assignmentType,
-                  _type: Entities.ROLE._type,
-                  _class: Entities.ROLE._class,
-                  id: role.id,
-                  description: role.description,
-                  name: role.label,
-                  displayName: role.label,
-                  roleType: role.type,
-                  status: role.status, //example: 'ACTIVE' or 'INACTIVE'
-                  created: parseTimePropertyValue(role.created)!,
-                  createdOn: parseTimePropertyValue(role.created)!,
-                  lastUpdated: parseTimePropertyValue(role.lastUpdated)!,
-                  lastUpdatedOn: parseTimePropertyValue(role.lastUpdated)!,
-                },
-              },
-            }),
-          );
+          roleEntity = await jobState.addEntity(createRoleEntity(role));
         }
 
         await jobState.addRelationship(
@@ -73,37 +74,9 @@ export async function fetchRoles({
     },
     async (group) => {
       await apiClient.iterateRolesByGroup(group._key, async (role) => {
-        let roleEntity = await jobState.findEntity(
-          Entities.ROLE._type + ':' + role.label + ':' + role.assignmentType,
-        );
+        let roleEntity = await jobState.findEntity(generateRoleKey(role));
         if (!roleEntity) {
-          roleEntity = await jobState.addEntity(
-            createIntegrationEntity({
-              entityData: {
-                source: role,
-                assign: {
-                  _key:
-                    Entities.ROLE._type +
-                    ':' +
-                    role.label +
-                    ':' +
-                    role.assignmentType,
-                  _type: Entities.ROLE._type,
-                  _class: Entities.ROLE._class,
-                  id: role.id,
-                  description: role.description,
-                  name: role.label,
-                  displayName: role.label,
-                  roleType: role.type,
-                  status: role.status, //example: 'ACTIVE' or 'INACTIVE'
-                  created: parseTimePropertyValue(role.created)!,
-                  createdOn: parseTimePropertyValue(role.created)!,
-                  lastUpdated: parseTimePropertyValue(role.lastUpdated)!,
-                  lastUpdatedOn: parseTimePropertyValue(role.lastUpdated)!,
-                },
-              },
-            }),
-          );
+          roleEntity = await jobState.addEntity(createRoleEntity(role));
         }
 
         await jobState.addRelationship(
